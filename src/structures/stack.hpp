3dc147cd1860fc0d7a1a653c1753bbe83d3fd8f8
Hamza <hamzasultan1096151005@gmail.com>
2024-10-19 12:33:50 +0500

    changed parameter types and return types to references -- added functions such as getters for capacity and current size and clear function

--- src/structures/stack.hpp
+++ src/structures/stack.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include<iostream>
 #include "int.h"
 
 namespace ds
@@ -12,13 +13,20 @@ struct Stack
     ~Stack();               // clear and de-allocate everything
 
     // push element on the top of stack, resize if stack is full
-    void push(T* elem);
+    void push(const T& elem);
 
     // pop the element from the top if stack is not empty
-    T* pop();
+    T& pop(bool& success=false);
 
     // get the top element of the stack
-    T* top() const;
+    T& top(bool& success=false) const;
+
+    // getter for capacity and size
+    ui32 get_capacity() const;
+    ui32 get_size() const;
+
+    // clear the stack; if deallocate is true, then clear the memory otherwise overwrite
+    void clear(bool deallocate=false);
 
     // see whether the stack is empty
     bool is_empty() const;
@@ -30,7 +38,6 @@ private:
     T* m_data{};            // The actual data
     ui32 m_top = -1;        // pointer/index to the top element
     ui32 m_capacity = 0;    // number of elements that can fit in the current allocated memory
-    
     // to resize the stack when capacity is reached
     void resize();
 };
@@ -48,6 +55,7 @@ template <typename T>
 Stack<T>::~Stack()
 {
     delete[] m_data;
+    m_data = nullptr;   // for case in which both destructor and clear are called to avoid double deletion
 }
 
 //..................................................................................................
@@ -66,36 +74,67 @@ void Stack<T>::resize()
 
 //..................................................................................................
 template <typename T>
-void Stack<T>::push(T* elem)
+void Stack<T>::push(const T& elem)
 {
     if (is_full())
     resize();       
-    m_data[++m_top] = *elem;
+    m_data[++m_top] = elem;
 }
 
 //..................................................................................................
 template <typename T>
-T* Stack<T>::pop()     
+T& Stack<T>::pop(bool& success)     
 {
     if (is_empty())
     {
-        std::cerr << "Stack is empty!" << std :: endl;
-        return nullptr;
+        success = false;
+        std::cerr << "Stack is empty! Cannot pop." << std :: endl;
+        return *reinterpret_cast<T*>(nullptr);  // return null reference
     }
-    return &m_data[m_top--];
+    success = true;
+    return m_data[m_top--];
 }
 
 //..................................................................................................
 template <typename T>
-T* Stack<T>::top() const     
+T& Stack<T>::top(bool& success) const     
 {
-    if (is_empty())
+    if(is_empty())
     {
-        std::cerr << "Stack is empty!" << std :: endl;
-        return nullptr;
+        success = false;
+        std::cerr << "Stack is empty! There is no top." << std :: endl;
+        return *reinterpret_cast<T*>(nullptr);  // return null reference
     }
-    return &m_data[m_top];
+    success = true;
+    return m_data[m_top];
 }   
+
+// ..................................................................................................
+template <typename T>
+ui32 Stack<T>::get_capacity() const 
+{
+    return m_capacity;
+}
+
+// ..................................................................................................
+template <typename T>
+ui32 Stack<T>::get_size() const 
+{
+    return m_top+1;
+}
+
+// ..................................................................................................
+template <typename T>
+void Stack<T>::clear(bool deallocate)
+{
+    if(deallocate)      
+    {
+        delete[] m_data;
+        m_data = nullptr;       // for case in which both destructor and clear are called to avoid double deletion
+        m_data = new T[m_capacity];
+    }
+    m_top = -1;
+}
 
 //..................................................................................................
 template <typename T>
